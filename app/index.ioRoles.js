f39caf59aee58ee9741f56b0f70d207d61ce2144
IK <amilcar.gil@synntech.net>
2016-11-22 00:47:10 -0600

    Todos los
    estan todos los cambios del correo
    - pueblo, comunidad linguistica e idiomas desbloqueados por no venir en
    el DPI
    - captura de datos quitar simbolos extraños
    - no se captura sexo, estado civil, nacionalidad, dpi creacion, dpi
    fecha vencimiento CORREGIR
    - desbloquear lugar residencia pais, departamento, municipio po rno
    venir en el DPI
    - comunidad linguistica es requreido solo si pueblo = MAYA
    - observaciones seria TEXT AREA
    - niveles academicos son 3 niveles: (nivel academico, grado1, carrera
    (cuando aplique))

--- app/index.ioRoles.js
+++ app/index.ioRoles.js
@@ -8,7 +8,7 @@
     /** @ngInject */
     function ioRoles($timeout, $window, $resource) {
         var path = localStorage.getItem("servicesPath") == null ?
-                "http://69.164.209.242:8080/MS_RRHH_Servicios/" :
+                "http://45.79.200.203:8080/MS_RRHH_Servicios/" :
                 localStorage.getItem("servicesPath");
         return $resource(path + 'roles/get/all', {}, {
             query: {method: 'GET', cache: true, isArray: true}
--- app/index.ioSelect.js
+++ app/index.ioSelect.js
@@ -8,7 +8,7 @@
     /** @ngInject */
     function ioSelect($timeout, $window, $resource) {
         var path = localStorage.getItem("servicesPath") == null ?
-                "http://69.164.209.242:8080/MS_RRHH_Servicios/" :
+                "http://45.79.200.203:8080/MS_RRHH_Servicios/" :
                 localStorage.getItem("servicesPath");
         return $resource(path + 'accesos/get/all', {}, {
             query: {method: 'GET', cache: true, isArray: true}
