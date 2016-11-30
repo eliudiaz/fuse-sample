e319a578684bccf595f6257363862ee28be7cf84
edcracken <eliudiaz@gmail.com>
2016-11-30 17:21:15 -0600

    timeout implemented

--- app/core/core.module.js
+++ app/core/core.module.js
@@ -1,36 +1,37 @@
 (function ()
 {
     'use strict';
-agGrid.initialiseAgGridWithAngular1(angular);
+    agGrid.initialiseAgGridWithAngular1(angular);
 
     angular
-        .module('app.core',
-            [
-                'ngAnimate',
-                'ngAria',
-                'ngCookies',
-                'ngMessages',
-                'ngResource',
-                'ngSanitize',
-                'ngMaterial',
-                'angular-chartist',
-                'chart.js',
-                'datatables',
-                'gridshore.c3js.chart',
-                'nvd3',
-                'pascalprecht.translate',
-                'timer',
-                'ui.router',
-                'uiGmapgoogle-maps',
-                'textAngular',
-                'ui.sortable',
-                'ng-sortable',
-                'xeditable',
-                'moment-picker',
-                'LocalStorageModule',
-                'agGrid',
-                'oi.select',
-                'ui-notification',
-                '720kb.datepicker'
-            ]);
+            .module('app.core',
+                    [
+                        'ngAnimate',
+                        'ngAria',
+                        'ngCookies',
+                        'ngMessages',
+                        'ngResource',
+                        'ngSanitize',
+                        'ngMaterial',
+                        'angular-chartist',
+                        'chart.js',
+                        'datatables',
+                        'gridshore.c3js.chart',
+                        'nvd3',
+                        'pascalprecht.translate',
+                        'timer',
+                        'ui.router',
+                        'uiGmapgoogle-maps',
+                        'textAngular',
+                        'ui.sortable',
+                        'ng-sortable',
+                        'xeditable',
+                        'moment-picker',
+                        'LocalStorageModule',
+                        'agGrid',
+                        'oi.select',
+                        'ui-notification',
+                        '720kb.datepicker',
+                        'ngIdle'
+                    ]);
 })();
