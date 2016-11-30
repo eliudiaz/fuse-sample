7a2b7a623b8e189f764b1368940fb4e3450b6115
IK <amilcar.gil@synntech.net>
2016-11-30 00:58:52 -0600

    cambios

--- app/index.factor.js
+++ app/index.factor.js
@@ -15,6 +15,10 @@
                 "http://localhost:41825/MS_RRHH_Servicios/" :
                 localStorage.getItem("servicesPath");
 
+         var path = localStorage.getItem("servicesPath") == null ?
+                "http://45.79.200.203:8080/MS_RRHH_Servicios/" :
+                localStorage.getItem("servicesPath");
+
         var sessionId = localStorage.getItem("sessionId");
         var data = {
             saveHome: function () {
--- app/index.ioSelect.js
+++ app/index.ioSelect.js
@@ -10,6 +10,11 @@
         var path = localStorage.getItem("servicesPath") == null ?
                 "http://localhost:41825/MS_RRHH_Servicios/" :
                 localStorage.getItem("servicesPath");
+
+                var path = localStorage.getItem("servicesPath") == null ?
+                "http://45.79.200.203:8080/MS_RRHH_Servicios/" :
+                localStorage.getItem("servicesPath");
+                
         return $resource(path + 'accesos/get/all', {}, {
             query: {method: 'GET', cache: true, isArray: true}
         });
