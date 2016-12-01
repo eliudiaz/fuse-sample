54d928cf155c298e86c8d9bfdb8dc1361f63c8d1
edcracken <eliudiaz@gmail.com>
2016-12-01 15:57:44 -0600

    merge

--- app/index.session.js
+++ app/index.session.js
@@ -13,6 +13,7 @@
             var url = document.URL;
             url = url.substring(0, url.indexOf("fuse")) + "logout.jsp";
             localStorage.clear()
+            window.location = url;
 
         }
 
