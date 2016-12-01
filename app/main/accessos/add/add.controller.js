5d87cd50ce0e66e30b2633aecec4f7b3434b5efc
edcracken <eliudiaz@gmail.com>
2016-12-01 15:51:31 -0600

    test

--- app/main/accessos/add/add.controller.js
+++ app/main/accessos/add/add.controller.js
@@ -3,18 +3,18 @@
     'use strict';
 
     angular
-            .module('app.accesos.add')
-            .controller('addController', addController);
+        .module('app.accessos.add')
+        .controller('addController', addController);
 
     /** @ngInject */
-    function addController($scope, $timeout, $mdDialog, $state, workSpace, localStorageService, ws) {
+  function addController($scope,$timeout,$mdDialog,$state,workSpace,localStorageService,ws){
         var vm = this;
 
         // Data
         vm.basicForm = {};
         var update = false;
-        var valor;
-
+        var valor; 
+        
         try {
             valor = localStorageService.get('workSpace').access.id;
         } catch (e) {
@@ -23,96 +23,96 @@
 
 
 
-        if (valor) {
+        if(valor){
             vm.basicForm.valor = localStorageService.get('workSpace').access.valor;
-            vm.basicForm.tipo = {id: localStorageService.get('workSpace').access.tipo, name: localStorageService.get('workSpace').access.tipo};
+            vm.basicForm.tipo = {id:localStorageService.get('workSpace').access.tipo,name:localStorageService.get('workSpace').access.tipo};
             update = true;
         }
 
 
-        $scope.getAllType = [{id: 'MENU', name: 'MENU'}, {id: 'ACCESS', name: 'ACCESS'}];
+        $scope.getAllType = [{id:'MENU',name:'MENU'},{id:'ACCESS',name:'ACCESS'}];
 
 
-        $scope.showAlert = function (ev) {
+        $scope.showAlert = function(ev) {
             $mdDialog.show(
-                    $mdDialog.alert()
-                    .parent(angular.element(document.querySelector('#popupContainer')))
-                    .clickOutsideToClose(true)
-                    .parent(angular.element(document.body))
-                    .title('Guardado')
-                    .textContent('Acceso Guardado Con Exito')
-                    .ariaLabel('Alert Dialog Demo')
-                    .ok('Aceptar')
-                    .targetEvent(ev)
-                    );
-        };
+              $mdDialog.alert()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .parent(angular.element(document.body))
+                .title('Guardado')
+                .textContent('Acceso Guardado Con Exito')
+                .ariaLabel('Alert Dialog Demo')
+                .ok('Aceptar')
+                .targetEvent(ev)
+            );
+          }; 
 
-        if (workSpace.user.id) {
+        if(workSpace.user.id){
             vm.basicForm.cui = workSpace.user.user;
         }
 
-        $scope.departamento = {
-            "base": [{id: 1, nombre: 'rol 1'}]
+        $scope.departamento ={
+          "base":[{id:1,nombre:'rol 1'}]
         };
 
 
 
-        $scope.save = function () {
+        $scope.save = function(){
             var obj = {
-                "valor": vm.basicForm.valor,
-                "tipo": vm.basicForm.tipo.id,
-                "creadoPor": "admin",
-                "fechaCreacion": "01-01-2012"
+              "valor":vm.basicForm.valor,
+              "tipo":vm.basicForm.tipo.id,
+              "creadoPor":"admin",
+              "fechaCreacion":"01-01-2012"
             };
 
-            if (update) {
+            if(update){
                 $scope.entryUp = ws.UpdateAccess(valor);
-                var EntryUp = $scope.entryUp.update(obj, function () {
-                    $scope.showAlert();
+                var EntryUp = $scope.entryUp.update(obj, function() {
+                    $scope.showAlert(); 
                     $state.go("app.accessos");
-                }, function (error) {
-                    workSpace.error = error.data.message;
-                    $scope.Error();
+                }, function(error) {
+                     workSpace.error = error.data.message; 
+                     $scope.Error();
                 });
-            } else {
+            }else{
                 $scope.entry = ws.saveAccess();
-                var entry = $scope.entry.save(obj, function () {
-                    $scope.showAlert();
+                var entry = $scope.entry.save(obj, function() {
+                    $scope.showAlert(); 
                     $state.go("app.accessos");
-                }, function (error) {
-                    workSpace.error = error.data.message;
-                    $scope.Error();
-                }
-                );
+                }, function(error) { 
+                     workSpace.error = error.data.message; 
+                     $scope.Error(); 
+                } 
+                );    
             }
+            
 
-
-
+            
         }
 
 
-        $scope.Error = function (id) {
+        $scope.Error = function(id) {
             $mdDialog.show({
-                controller: function ($scope, $mdDialog, workSpace) {
-                    $scope.closeDialog = function () {
+                controller: function($scope, $mdDialog, workSpace) {
+                    $scope.closeDialog = function() {
                         $mdDialog.hide();
                     }
                 },
                 template: '<md-dialog>' +
-                        ' <md-dialog-content>' +
-                        ' Error:  ' + workSpace.error + '' +
-                        '</md-dialog-content>' +
-                        '  <md-dialog-actions>' +
-                        '    <md-button ng-click="closeDialog()" class="md-primary">' +
-                        '      Close' +
-                        '    </md-button>' +
-                        '  </md-dialog-actions>' +
-                        '</md-dialog>',
+                    ' <md-dialog-content>' +
+                    ' Error:  '+ workSpace.error +'' +
+                    '</md-dialog-content>' +
+                    '  <md-dialog-actions>' +
+                    '    <md-button ng-click="closeDialog()" class="md-primary">' +
+                    '      Close' +
+                    '    </md-button>' +
+                    '  </md-dialog-actions>' +
+                    '</md-dialog>',
                 parent: angular.element('body'),
                 clickOutsideToClose: true
             });
-        }
+        }    
 
-    }
+  }
 })();
 
