de1ca7c94a5884eff07911cabdcc70cd2872e941
edcracken <eliudiaz@gmail.com>
2016-12-18 23:20:32 -0600

    module title fixed

--- app/main/home/home.module.js
+++ app/main/home/home.module.js
@@ -3,40 +3,40 @@
     'use strict';
 
     angular
-        .module('app.home', [])
-        .config(config);
+            .module('app.home', [])
+            .config(config);
 
     /** @ngInject */
     function config($stateProvider, $translatePartialLoaderProvider, msNavigationServiceProvider)
     {
         // State
         $stateProvider
-            .state('app.home', {
-                url    : '/home',
-                views  : {
-                    'content@app': {
-                        templateUrl: 'app/main/home/home.html',
-                        controller : 'homeController as vm'
+                .state('app.home', {
+                    url: '/home',
+                    views: {
+                        'content@app': {
+                            templateUrl: 'app/main/home/home.html',
+                            controller: 'homeController as vm'
+                        }
                     }
-                }
-            });
+                });
 
         // Translation
         $translatePartialLoaderProvider.addPart('app/main/home');
 
         // Navigation
         msNavigationServiceProvider.saveItem('fuse', {
-            title : '',
-            group : true,
+            title: '',
+            group: true,
             weight: 1
         });
 
         msNavigationServiceProvider.saveItem('fuse.home', {
-            title    : 'Home',
-            icon     : 'icon-tile-four',
-            state    : 'app.home',
-            translate: 'Home',
-            weight   : 1
+            title: 'Inicio',
+            icon: 'icon-tile-four',
+            state: 'app.home',
+            translate: 'Inicio',
+            weight: 1
         });
     }
 })();
